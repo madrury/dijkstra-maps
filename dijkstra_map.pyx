5c75c07204d57e9d94d2c880e79ad14d3323d159
madrury <matthew.drury.83@gmail.com>
2018-04-27 09:17:23 -0700

    Add python interface layer.

--- dijkstra_map.pyx
+++ dijkstra_map.pyx
@@ -2,33 +2,44 @@ import numpy as np
 cimport numpy as np
 cimport cython
 
-def build_map(long [:, :] dmap, np.uint8_t [:, :] walkable, 
-              long initial=999):
+
+def build_map(dmap, walkable, initial=999):
+    dmap = dmap.astype(np.float64, copy=False)
+    walkable = walkable.astype(np.uint8, copy=False)
+    return _build_map(dmap, walkable, initial)
+
+
+def _build_map(double [:, :] dmap,
+               np.uint8_t [:, :] walkable, 
+               double initial=999):
     cdef bint updated = True
     while updated:
-        updated = update(dmap, walkable, initial)
+        updated = _update_map(dmap, walkable, initial)
     return np.array(dmap)
 
-@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.boundscheck(False)
 @cython.wraparound(False)
-cdef bint update(long [:, :] dmap, np.uint8_t [:, :] walkable, 
-           long initial=999):
+cdef bint _update_map(double [:, :] dmap,
+                      np.uint8_t [:, :] walkable, 
+                      double initial=999):
     cdef Py_ssize_t i, j
     cdef bint updated = False
     for i in range(dmap.shape[0]):
         for j in range(dmap.shape[1]):
             if walkable[i, j] and dmap[i, j] == initial:
-                updated = update_entry(i, j, dmap, initial) or updated
+                updated = _update_entry(i, j, dmap, initial) or updated
     return updated 
 
-@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.boundscheck(False)
 @cython.wraparound(False)
-cdef bint update_entry(Py_ssize_t i, Py_ssize_t j, long [:, :] dmap, long initial):
-    cdef long u = initial
-    cdef long d = initial
-    cdef long l = initial
-    cdef long r = initial
-    cdef long new = initial
+cdef bint _update_entry(Py_ssize_t i, Py_ssize_t j,
+                        double [:, :] dmap,
+                        double initial):
+    cdef double u = initial
+    cdef double d = initial
+    cdef double l = initial
+    cdef double r = initial
+    cdef double new = initial
 
     if i > 0 and dmap[i - 1, j] != initial:
         l = dmap[i - 1, j] + 1
