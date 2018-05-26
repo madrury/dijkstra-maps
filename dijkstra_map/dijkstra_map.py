c25ca622567b18e5818724e89f83980618beb1fd
madrury <matthew.drury.83@gmail.com>
2018-05-26 13:44:06 -0700

    Add python api.

---
+++ dijkstra_map/dijkstra_map.py
@@ -0,0 +1,19 @@
+import numpy as np
+from _dijkstra_map import build_map
+
+
+class DijskstraMap:
+
+    def __init__(self, walkable, initial=999):
+        self.walkable = walkable.astype(np.uint8)
+        self.dmap = np.full(self.walkable.shape, initial).astype(np.float)
+        self.initial = initial
+
+    def build(self):
+        self.dmap = build_map(self.dmap, self.walkable, initial=self.initial)
+
+    def set_source(self, position, distance=0):
+        x, y = position
+        if not self.walkable[x, y]:
+            raise ValueError("Cannot set value in dmap at non-walkable space")
+        self.dmap[x, y] = distance
--- dijkstra_map/test/test_dijkstra.py
+++ dijkstra_map/test/test_dijkstra.py
@@ -1,8 +1,28 @@
 import pytest
 import numpy as np
+
+from dijkstra_map.dijkstra_map import DijskstraMap
 from _dijkstra_map import build_map
 
+
 def test_sink():
+    walkable = np.ones((5, 5)).astype(np.uint8)
+    dm = DijskstraMap(walkable)
+    dm.set_source((2, 2))
+    dm.build()
+    result = np.array([[2, 2, 2, 2, 2],
+                       [2, 1, 1, 1, 2], 
+                       [2, 1, 0, 1, 2], 
+                       [2, 1, 1, 1, 2],
+                       [2, 2, 2, 2, 2]])
+    assert np.all(dm.dmap == result)
+
+
+
+#-----------------------------------------------------------------------------
+# Private C api
+#-----------------------------------------------------------------------------
+def test_sink_c():
     dmap = np.full((5, 5), 999)
     dmap[2, 2] = 0
     walkable = np.ones((5, 5)).astype(np.uint8)
@@ -14,7 +34,7 @@ def test_sink():
     dmap = build_map(dmap, walkable) 
     assert np.all(dmap == result)
 
-def test_two_sinks():
+def test_two_sinks_c():
     dmap = np.full((3, 5), 999)
     dmap[1, 1] = 0
     dmap[1, 3] = 0
@@ -25,7 +45,7 @@ def test_two_sinks():
     dmap = build_map(dmap, walkable) 
     assert np.all(dmap == result)
 
-def test_corridor():
+def test_corridor_c():
     dmap = np.full((3, 5), 999)
     dmap[1, 1] = 0
     walkable = np.zeros((3, 5)).astype(np.uint8)
@@ -36,7 +56,7 @@ def test_corridor():
     dmap = build_map(dmap, walkable) 
     assert np.all(dmap == result)
 
-def test_cross():
+def test_cross_c():
     dmap = np.full((7, 7), 999)
     dmap[3, 3] = 0
     walkable = np.zeros((7, 7)).astype(np.uint8)
